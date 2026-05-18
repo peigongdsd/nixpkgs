37372240acd9d0623f17436823c4b65f13413050
peigongdsd <51317171+peigongdsd@users.noreply.github.com>
2026-05-18 22:23:14 +0100

    Revert " intel-oneapi-toolkit: init at 2026.0.0.198, replace intel-oneapi.{base,hpc} "

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1642,6 +1642,8 @@ with pkgs;
 
   coreboot-configurator = libsForQt5.callPackage ../tools/misc/coreboot-configurator { };
 
+  intel-oneapi = recurseIntoAttrs (callPackage ../development/libraries/intel-oneapi { });
+
   intelLlvmStdenv = intel-llvm.stdenv;
 
   cambrinary = python3Packages.callPackage ../applications/misc/cambrinary { };
